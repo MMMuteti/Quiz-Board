661e202b58e2aa812780b20feaed8efa14886d23
Moses Mbugua <mmmuteti@gmail.com>
2023-10-12 17:38:15 +0300

    add variables and score function

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,16 @@ var result5 = 5;
 function score () {
   return "you have scored" + total;
 }
+
+$(document).ready(function() {
+    $("#button2").click(function() {
+      var question1 = $("input[name='question1']:checked").val();
+      var question2 = $("input[name='question2']:checked").val();
+      var question3 = $("input[name='question3']:checked").val();
+      var question4 = $("input[name='question4']:checked").val();
+      var question5 = $("input[name='question5']:checked").val();
+      console.log(question1);
+      console.log(question2);
+      console.log(question3);
+      console.log(question4);
+      console.log(question5);
